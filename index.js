0dd977963f8a2818d8a621d32279bc5744180ec4
nicolas-saikali <nicolas@itxi.net>
2023-01-13 18:26:27 +0200

    v2.0.4: reimprove isElementInViewport()

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ function isElementInViewport(elt) {
     const rect = elt.getBoundingClientRect();
     
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+        rect.top <= window.screenTop + window.innerHeight && 
+        rect.top >= window.screenTop || 
+        rect.bottom <= window.screenTop + window.innerHeight && 
+        rect.bottom >= window.screenTop
     );
 
 }
